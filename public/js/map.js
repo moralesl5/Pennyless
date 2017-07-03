9eda437cb2ca18f264303ec75e531dcd7b3c5d02
moralesl5 <loganjmorales@gmail.com>
2017-07-03 19:08:59 -0400

    Google maps working ✅

--- public/js/map.js
+++ public/js/map.js
@@ -2,29 +2,32 @@ $(() => {
 
 	console.log('script running');
 
-	const lat = $('.lat').attr('data_id');
-	const long = $('.long').attr('data_id')
-    console.log(`Lat: ${lat}`);
-    console.log(`Long: ${long}`);
+	const lat = parseFloat($('.lat').attr('data_id'));
+	const long = parseFloat($('.long').attr('data_id'));
+    console.log(lat);
+    console.log(long);
 
     function initMap() {
-        var uluru = {
+
+        let center = {
             lat: 40.739885,
             lng: -73.990082
         };
-        // var marker1 = {
-        //     lat: document.getElementById('lat').value(),
-        //     long: document.getElementById('long').value()
-        // }
+        var marker1 = {
+            lat: lat,
+            lng: long
+        };
         var map = new google.maps.Map(document.getElementById('map'), {
             zoom: 15,
-            center: uluru
+            center: center
         });
-        // var marker = new google.maps.Marker({
-        //     position: marker1,
-        //     map: map
-        // });
+        var marker = new google.maps.Marker({
+            position: marker1,
+            map: map
+        });
     }
+
+    initMap();
 
 
 
